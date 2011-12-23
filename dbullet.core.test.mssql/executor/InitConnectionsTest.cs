07a3fb6a23f3f5e3c4d3f3f1f21ae5dc291224de
Semen Kirushin <deltasem@gmail.com>
2011-12-23 10:15:22 +0400

    =Tests for mssql server

---
+++ dbullet.core.test.mssql/executor/InitConnectionsTest.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="InitConnectionsTest.cs" company="delta" created="23.12.2011">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using NUnit.Framework;
+
+namespace dbullet.core.test.mssql.executor
+{
+	/// <summary>
+	/// Database initialize tests
+	/// </summary>
+	[TestFixture]
+	public class InitConnectionsTest
+	{
+		/// <summary>
+		/// Database initialize
+		/// </summary>
+		[TestFixtureSetUp]
+		public void Init()
+		{
+		}
+	}
+}
